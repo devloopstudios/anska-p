4f779dbe565927e93ab06975647a631b3dc7182a
lucasmence <lucas21021996@gmail.com>
2018-12-20 22:09:35 -0200

    Updated core and created the jsonLib with the manipulation of the json Files;

--- source/core.py
+++ source/core.py
@@ -1,2 +1,36 @@
+import jsonLib
 
-#last time updated december, 17 of 2018
+class Reader:
+    def __init__(self):
+        self.language = 'en_us'
+
+        self.fileInput = '../input/input.json'
+        self.fileOutput = '../output/output.json'
+        self.fileRequest = '../data/'+self.language+'/request_types.json'
+        self.fileReply = '../data/'+self.language+'/reply_types.json'
+
+        self.fieldInput = 'text'
+        self.fieldOutput = 'text'
+
+        self.defaultEmptyValue = "nullbot"
+    
+    def readRequest(self):
+        self.requestValue = jsonLib.getValue(self.fileInput,self.fieldInput,self.defaultEmptyValue)
+
+    def requestTypeSearch(self):
+        self.requestType = jsonLib.getColumnByData(self.fileRequest,self.requestValue,self.defaultEmptyValue) 
+    
+    def replySearch(self):
+        self.replyValue = jsonLib.getValue(self.fileReply,self.requestType,self.defaultEmptyValue)  
+
+    def process(self):
+        print("hello world")  
+
+
+        
+
+print(jsonLib.getValue('../input/input.json','text','nothing here'));
+
+jsonLib.setValue('../output/output.json','text','hi');
+
+
---
+++ source/jsonLib.py
@@ -0,0 +1,45 @@
+import json
+
+def getValue(path, field, default):
+
+    value = None
+    try:    
+        with open(path, 'r') as file:
+            jsonFile = json.load(file)  
+
+        value = format(jsonFile[field])
+    except:
+        return default
+
+    if (value is None):        
+        return  default
+    else:
+        return value
+
+def getColumnByData(path, value):
+    #todo : finish here catching arrays
+    value = None
+    try:    
+        with open(path, 'r') as file:
+            jsonFile = json.load(file)  
+
+        value = format(jsonFile[field])
+    except:
+        return default
+
+    if (value is None):        
+        return  default
+    else:
+        return value   
+
+
+def setValue(path, field, value):
+
+    jsonFile = None
+
+    with open(path, 'r') as file:
+        jsonFile = json.load(file) 
+        jsonFile[field] = value
+
+    with open(path, 'w') as file:      
+        json.dump(jsonFile, file)
