26f244c39100b5533edbd8b2a76238186038d699
Mence <lucas21021996@gmail.com>
2019-01-29 14:28:51 -0200

    Removed the Reader class ;

--- source/core.py
+++ source/core.py
@@ -1,47 +1,6 @@
-import jsonLib
+import readerClass
 
-class Reader:
-    def __init__(self):
-        self.language = 'en_us'
-
-        self.fileInput = '../input/input.json'
-        self.fileOutput = '../output/output.json'
-        self.fileRequest = '../data/'+self.language+'/request_types.json'
-        self.fileReply = '../data/'+self.language+'/reply_types.json'
-
-        self.fieldInput = 'text'
-        self.fieldOutput = 'text'
-
-        self.defaultEmptyValue = 'null'
-        self.messageFinish = 'Done'
-
-        self.positionListValue = -1
-        
-        self.jsonManager = JsonManager()
-    
-    def __del__(self):
-        del jsonManager
-        
-    def readRequest(self):
-        self.requestValue = jsonManager.getValue(self.fileInput, self.fieldInput, self.defaultEmptyValue)
-
-    def requestTypeSearch(self):
-        self.requestType = jsonManager.getColumnByData(self.fileRequest, self.requestValue, self.defaultEmptyValue) 
-    
-    def replySearch(self):
-        self.replyValue = jsonManager.getValueFromList(self.fileReply, self.requestType, self.defaultEmptyValue, self.positionListValue)  
-
-    def replyWrite(self):
-        jsonManager.setValue(self.fileOutput, self.fieldOutput, self.replyValue)
-
-    def process(self):
-        self.readRequest() 
-        self.requestTypeSearch()
-        self.replySearch()
-        self.replyWrite()
-        print(self.messageFinish)
-
-reader = Reader()
+reader = readerClass.Reader()
 reader.process()
 del reader
 
