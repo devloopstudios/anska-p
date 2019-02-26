29fd7a951b81a73680c7634de32384791ab2a451
lucasmence <lucas21021996@gmail.com>
2019-02-26 20:33:24 -0300

    Added test w/ mysql database;

--- source/core.py
+++ source/core.py
@@ -1,11 +1,31 @@
 import readerClass
 import sys
+import MySQLdb
 
-reader = readerClass.Reader()
-if (len(sys.argv) > 1):
-    reader.setCommand(sys.argv[1])
-reader.process()
+#realizar refactoring migrando do json para o DB
 
-del reader
+db = MySQLdb.connect(host="192.168.0.20",    
+                     user="mence",        
+                     passwd="21021996",  
+                     db="anska")       
+
+
+cur = db.cursor()
+
+
+cur.execute("SELECT * FROM BOT_REQUEST")
+
+
+for row in cur.fetchall():
+    print row[1] + row[2]
+
+db.close()
+
+#reader = readerClass.Reader()
+#if (len(sys.argv) > 1):
+#    reader.setCommand(sys.argv[1])
+#reader.process()
+
+#del reader
 
 
